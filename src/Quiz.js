896e472083ffc22764194ef9ffff3730a2d9e7f0
chavamf <chavamf@gmail.com>
2018-09-06 13:26:18 -0400

    Module 2: Creaating a Quiz Component

---
+++ src/Quiz.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+let quizData = require('./quiz_data.json');
+
+export class Quiz extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			quiz_position: 1
+		};
+	}
+	
+	render() {
+		return (
+			<React.Fragment>
+				<div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Quiz;
